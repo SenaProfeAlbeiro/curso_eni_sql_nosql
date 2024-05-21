c7ef6fb4a198dd3405401d530897cd275598b8ee
SenaProfeAlbeiro <earamos42@misena.edu.co>
2024-05-21 10:14:52 -0500

    Soluciones a las sesiones

--- projects/solutions/app12/android/app/src/main/kotlin/com/example/proyecto001/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.proyecto001
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- projects/solutions/app13/android/app/src/main/kotlin/com/example/proyecto002/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.proyecto002
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
